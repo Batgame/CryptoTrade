a562e1d90587d3ffb3fa034d22895baf6d960f46
Bastien <43323819+Batgame@users.noreply.github.com>
2021-01-18 22:04:40 +0100

    Update historique.php

--- historique.php
+++ historique.php
@@ -1,186 +1,186 @@
 <!DOCTYPE html>
 <html lang="fr">
-   <head>
-      <title>L'histoire de la crytomonnaie</title>
-      <meta charset="utf-8">
-      <link rel="stylesheet" type="text/css" href="css/historiques.css">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css" />
-      <link rel="icon" href="images/favicon.ico">
-   </head>
-   <body>
-      <header>
-         <div id="petite_bande_bleue">
-            <marquee scrollamount="10" width="100%">
-               <p>Cryptotrade et ses concepteurs ont besoins de votre soutien!</p>
-            </marquee>
-         </div>
-         <div id="navigation-header">
-            <nav>
-               <ul>
-                  <li>
-                     <a href="#">Historique</a> 
-                     <ul>
-                        <li><a href="historique.php">Dates clés</a></li>
-                        <li><a href="crypto.html">Qu’est ce que la crypto monnaie?</a></li>
-                     </ul>
-                  </li>
-                  <li class ="exception">
-                     <a>Comment ça marche ?</a>
-                     <ul>
-                        <li><a href="platform.html">Plateforme d’échanges</a></li>
-                        <li><a href="legislation.html">Législation</a></li>
-                     </ul>
-                  </li>
-                  <li>
-                     <a>Actualités</a>
-                     <ul>
-                        <li><a href="cours.php">Cours des cryptos</a></li>
-                        <li><a href="#">Crypto-news</a></li>
-                     </ul>
-                  </li>
-                  <li>
-                     <a>Ressources</a>
-                     <ul>
-                        <li><a href="vocabulaire.php">Vocabulaire</a></li>
-                     </ul>
-                  </li>
-               </ul>
-               <a id="logo-site-header" href="index.html"><img src="images/logoNoir.png"></a>
-            </nav>
-            <div class="Co">
-               <a id="connexion" href="login.php">Connexion</a>
-               <a id ="demarre" href="debute.php">Démarrer</a>
+    <head>
+        <title>L'histoire de la crytomonnaie</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="css/historiques.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css" />
+        <link rel="icon" href="images/favicon.ico">
+    </head>
+    <body>
+        <header>
+            <div id="petite_bande_bleue">
+                <marquee scrollamount="10" width="100%">
+                    <p>Cryptotrade et ses concepteurs ont besoins de votre soutien!</p>
+                </marquee>
             </div>
-         </div>
-      </header>
-      <div id="banderole">
-         <h1><i class="fas fa-history"></i> L'historique de la cryptomonnaie</h1>
-      </div>
-      </div>
-      <div class="timeline">
-         <div class="partie gauche">
-            <div class="contenu">
-               <time datetime="1989"  class="date">1989</time>
-               <p>Création de DigiCash Inc. par David Chaum dans le but de créer la première crypto-monnaie utilisée dans le monde entier. (Echec)
-               </p>
+            <div id="navigation-header">
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="#">Historique</a> 
+                            <ul>
+                                <li><a href="historique.php">Dates clés</a></li>
+                                <li><a href="crypto.html">Qu’est ce que la crypto monnaie?</a></li>
+                            </ul>
+                        </li>
+                        <li class ="exception">
+                            <a>Comment ça marche ?</a>
+                            <ul>
+                                <li><a href="platform.html">Plateforme d’échanges</a></li>
+                                <li><a href="legislation.html">Législation</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a>Actualités</a>
+                            <ul>
+                                <li><a href="cours.php">Cours des cryptos</a></li>
+                                <li><a href="#">Crypto-news</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a>Ressources</a>
+                            <ul>
+                                <li><a href="vocabulaire.php">Vocabulaire</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <a id="logo-site-header" href="index.html"><img src="images/logoNoir.png"></a>
+                </nav>
+                <div class="Co">
+                    <a id="connexion" href="login.php">Connexion</a>
+                    <a id ="demarre" href="debute.php">Démarrer</a>
+                </div>
             </div>
-         </div>
-         <div class="partie droite">
-            <div class="contenu">
-               <time datetime="2009-01-03"  class="date">3 Janvier 2009</time>
-               <p>Création du <b>Bitcoin</b> par une personne utilisant le peudonyme de Satoshi Nakamoto en utilisant la technologie de la <b>Blockchain</b>.
-               </p>
+        </header>
+        <div id="banderole">
+            <h1><i class="fas fa-history"></i> L'historique de la cryptomonnaie</h1>
+        </div>
+        </div>
+        <div class="timeline">
+            <div class="partie gauche">
+                <div class="contenu">
+                    <time datetime="1989"  class="date">1989</time>
+                    <p>Création de DigiCash Inc. par David Chaum dans le but de créer la première crypto-monnaie utilisée dans le monde entier. (Echec)
+                    </p>
+                </div>
             </div>
-         </div>
-         <div class="partie gauche">
-            <div class="contenu">
-               <time datetime="2010-05-22"  class="date">22 Mai 2010</time>
-               <p>Première utilisation de crypto-monnaie au monde. Laszlo Hanyecz s’offre deux pizzas pour 10 000 BTC.</p>
+            <div class="partie droite">
+                <div class="contenu">
+                    <time datetime="2009-01-03"  class="date">3 Janvier 2009</time>
+                    <p>Création du <b>Bitcoin</b> par une personne utilisant le peudonyme de Satoshi Nakamoto en utilisant la technologie de la <b>Blockchain</b>.
+                    </p>
+                </div>
             </div>
-         </div>
-         <div class="partie droite">
-            <div class="contenu">
-               <time datetime="2011-02-09"  class="date">9 Février 2011</time>
-               <p>Le <b>Bitcoin</b> atteint pour la première fois une valeur équivalente au dollar américain.
-               </p>
+            <div class="partie gauche">
+                <div class="contenu">
+                    <time datetime="2010-05-22"  class="date">22 Mai 2010</time>
+                    <p>Première utilisation de crypto-monnaie au monde. Laszlo Hanyecz s’offre deux pizzas pour 10 000 BTC.</p>
+                </div>
             </div>
-         </div>
-         <div class="partie gauche">
-            <div class="contenu">
-               <time datetime="2012"  class="date">2012</time>
-               <p>Lancement de <b>Ripple</b>, un réseau de transaction utilisant une nouvelle crypto-devise, le Ripple.</p>
+            <div class="partie droite">
+                <div class="contenu">
+                    <time datetime="2011-02-09"  class="date">9 Février 2011</time>
+                    <p>Le <b>Bitcoin</b> atteint pour la première fois une valeur équivalente au dollar américain.
+                    </p>
+                </div>
             </div>
-         </div>
-         <div class="partie droite">
-            <div class="contenu">
-               <time datetime="2013-12"  class="date">Décembre 2013</time>
-               <p>Vitalik Buterin décrit son projet <b>Ethereum</b>. Il met en vente les premiers ethers pour financer son projet.</p>
+            <div class="partie gauche">
+                <div class="contenu">
+                    <time datetime="2012"  class="date">2012</time>
+                    <p>Lancement de <b>Ripple</b>, un réseau de transaction utilisant une nouvelle crypto-devise, le Ripple.</p>
+                </div>
             </div>
-         </div>
-         <div class="partie gauche">
-            <div class="contenu">
-               <time datetime="2015-02"  class="date">Février 2015</time>
-               <p> Plus de 100 000 commerçants acceptent le bitcoin dont Microsoft, Dell, Twitch…</p>
+            <div class="partie droite">
+                <div class="contenu">
+                    <time datetime="2013-12"  class="date">Décembre 2013</time>
+                    <p>Vitalik Buterin décrit son projet <b>Ethereum</b>. Il met en vente les premiers ethers pour financer son projet.</p>
+                </div>
             </div>
-         </div>
-         <div class="partie droite">
-            <div class="contenu">
-               <time datetime="2016-01"  class="date">Janvier 2016</time>
-               <p>La Banque Populaire de Chine annonce lancer sa propre crypto-monnaie.</p>
+            <div class="partie gauche">
+                <div class="contenu">
+                    <time datetime="2015-02"  class="date">Février 2015</time>
+                    <p> Plus de 100 000 commerçants acceptent le bitcoin dont Microsoft, Dell, Twitch…</p>
+                </div>
             </div>
-         </div>
-         <div class="partie gauche">
-            <div class="contenu">
-               <time datetime="2016-04"  class="date">Avril 2016</time>
-               <p>Bitstamp s’installe en Belgique et devient la première plateforme d’échanges BTC/EUR à obtenir une licence nationale.
-               </p>
+            <div class="partie droite">
+                <div class="contenu">
+                    <time datetime="2016-01"  class="date">Janvier 2016</time>
+                    <p>La Banque Populaire de Chine annonce lancer sa propre crypto-monnaie.</p>
+                </div>
             </div>
-         </div>
-         <div class="partie droite">
-            <div class="contenu">
-               <time datetime="2017-05"  class="date">Mai 2017</time>
-               <p>Plus de 1000 crypto-monnaies sur le marché.</p>
+            <div class="partie gauche">
+                <div class="contenu">
+                    <time datetime="2016-04"  class="date">Avril 2016</time>
+                    <p>Bitstamp s’installe en Belgique et devient la première plateforme d’échanges BTC/EUR à obtenir une licence nationale.
+                    </p>
+                </div>
             </div>
-         </div>
-         <div class="partie gauche">
-            <div class="contenu">
-               <time datetime="2018-01"  class="date">Janvier 2018</time>
-               <p>Près de 1400 crypto-monnaies sur le marché.</p>
+            <div class="partie droite">
+                <div class="contenu">
+                    <time datetime="2017-05"  class="date">Mai 2017</time>
+                    <p>Plus de 1000 crypto-monnaies sur le marché.</p>
+                </div>
             </div>
-         </div>
-         <div class="partie droite">
-            <div class="contenu">
-               <time datetime="2020-01"  class="date">Janvier 2020</time>
-               <p> Nouveau record pour le bitcoin qui dépasse pour la première fois de son histoire les 40 000 dollars américains.</p>
+            <div class="partie gauche">
+                <div class="contenu">
+                    <time datetime="2018-01"  class="date">Janvier 2018</time>
+                    <p>Près de 1400 crypto-monnaies sur le marché.</p>
+                </div>
             </div>
-         </div>
-      </div>
-      <div id="footer-haut">
-         <div id="footer-haut-haut">
-            <a id="logo-site-footer" href="index.html"><img src="images/logo.png"></a>
-         </div>
-         <div id="footer-haut-bas">
-            <div id="partie-don">
-               <p>Soutenez-nous! <a href="#">Donnez</a></p>
+            <div class="partie droite">
+                <div class="contenu">
+                    <time datetime="2020-01"  class="date">Janvier 2020</time>
+                    <p> Nouveau record pour le bitcoin qui dépasse pour la première fois de son histoire les 40 000 dollars américains.</p>
+                </div>
             </div>
-            <div id="navigation-footer">
-               <nav>
-                  <ul>
-                     <li>
-                        <a class="titre-menu">Historique</a> 
+        </div>
+        <div id="footer-haut">
+            <div id="footer-haut-haut">
+                <a id="logo-site-footer" href="index.html"><img src="images/logo.png"></a>
+            </div>
+            <div id="footer-haut-bas">
+                <div id="partie-don">
+                    <p>Soutenez-nous! <a href="#">Donnez</a></p>
+                </div>
+                <div id="navigation-footer">
+                    <nav>
                         <ul>
-                           <li><a class="element-menu" href="historique.php">Dates clés</a></li>
-                           <li><a class="element-menu" href="crypto.html">Qu’est ce que la crypto monnaie?</a></li>
+                            <li>
+                                <a class="titre-menu">Historique</a> 
+                                <ul>
+                                    <li><a class="element-menu" href="historique.php">Dates clés</a></li>
+                                    <li><a class="element-menu" href="crypto.html">Qu’est ce que la crypto monnaie?</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a class="titre-menu">Comment ça marche ?</a>
+                                <ul>
+                                    <li><a  class="element-menu"href="platform.html">Plateforme d’échanges</a></li>
+                                    <li><a class="element-menu"href="legislation.html">Législation</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a class="titre-menu">Actualités</a>
+                                <ul>
+                                    <li><a class="element-menu" href="cours.php">Cours des cryptos</a></li>
+                                    <li><a class="element-menu" href="#">Crypto-news</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a class="titre-menu">Ressources</a>
+                                <ul>
+                                    <li><a class="element-menu" href="vocabulaire.php">Vocabulaire</a></li>
+                                </ul>
+                            </li>
                         </ul>
-                     </li>
-                     <li>
-                        <a class="titre-menu">Comment ça marche ?</a>
-                        <ul>
-                           <li><a  class="element-menu"href="platform.html">Plateforme d’échanges</a></li>
-                           <li><a class="element-menu"href="legislation.html">Législation</a></li>
-                        </ul>
-                     </li>
-                     <li>
-                        <a class="titre-menu">Actualités</a>
-                        <ul>
-                           <li><a class="element-menu" href="cours.php">Cours des cryptos</a></li>
-                           <li><a class="element-menu" href="#">Crypto-news</a></li>
-                        </ul>
-                     </li>
-                     <li>
-                        <a class="titre-menu">Ressources</a>
-                        <ul>
-                           <li><a class="element-menu" href="vocabulaire.php">Vocabulaire</a></li>
-                        </ul>
-                     </li>
-                  </ul>
-               </nav>
+                    </nav>
+                </div>
             </div>
-         </div>
-      </div>
-      <div id="footer-bas">
-         <span id="copy-right">&copy;Pong industry 2021</span>
-      </div>
-      </footer>
-   </body>
+        </div>
+        <div id="footer-bas">
+            <span id="copy-right">&copy;Pong industry 2021</span>
+        </div>
+        </footer>
+    </body>
 </html>
